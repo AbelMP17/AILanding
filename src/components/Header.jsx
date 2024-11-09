c2c6c50e071e6e002756524972022251110e7869
AbelMP17 <abelmp890@gmail.com>
2024-11-09 19:47:40 +0100

    Sign in and New Account button colors (Mobile) 2.0

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -60,8 +60,8 @@ const Header = () => {
                 key={item.id}
                 href={item.url}
                 onClick={handleClick}
-                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
-                  item.onlyMobile ? "lg:hidden" : "text-color-1/60"
+                className={`block relative font-code text-2xl uppercase  transition-colors hover:text-color-1 ${
+                  item.onlyMobile ? "lg:hidden text-color-6/80" : "text-n-1"
                 } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
                   item.url === pathname.hash
                     ? "z-2 lg:text-n-1"
